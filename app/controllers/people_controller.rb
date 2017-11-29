aff5b40da4c6e7a18c18ae601c11491d1a15aeaa
Anthony Kelley <anthonymkelley@gmail.com>
2017-11-28 22:49:23 -0700

    add validate function

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,11 +12,38 @@ class PeopleController < ApplicationController
   end
 
   def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to people_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(person_params)
+      redirect_to person_path(@person)
+    else
+      render :edit
+    end    
   end
 
   def edit
   end
 
   def destroy
+    Person.find(params[:id]).destroy
+    redirect_to people_path
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :age, :hair_color, :eye_color, :gender, :alive)
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ApplicationRecord
+  validates :name, length: { minimum: 3 }
+  validates :age, numericality: true
 end
+
+# could not get message to work
